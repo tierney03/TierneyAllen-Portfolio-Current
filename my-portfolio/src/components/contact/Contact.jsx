7d5a1a048f70a7349f204f63abb6c32b40964b38
tierney03 <tierney_03@mac.com>
2023-01-15 17:33:57 -0600

    contact me

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import "./contact.scss";
+import HandshakeIcon from "@mui/icons-material/Handshake";
 
-function Contact() {
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <HandshakeIcon className="handshake" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && (
+            <span>Thank you for reaching out. We will be in touch soon!</span>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
-
-export default Contact;
