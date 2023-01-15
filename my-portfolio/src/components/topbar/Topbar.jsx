486f369449d437833a037f33e5b685132295dea9
tierney03 <tierney_03@mac.com>
2023-01-15 09:43:54 -0600

    left of topbar

--- my-portfolio/src/components/topbar/Topbar.jsx
+++ my-portfolio/src/components/topbar/Topbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./topbar.scss";
+import { Face3, Mail } from "@mui/icons-material";
 
 function Topbar() {
   return (
     <div className="topbar" id="subsection">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">See this!</a>
+          <a href="#intro" className="logo">
+            Tierney
+          </a>
+          <div className="itemContainer">
+            <Face3 className="icon" />
+            <span>1(360)999-6724</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>tierney_03@mac.com</span>
+          </div>
+          <div className="right"></div>
         </div>
-        <div className="right"></div>
       </div>
     </div>
   );
