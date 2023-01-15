01b617e9133d07d5d4aff081ca7bc8fa0f782f1b
tierney03 <tierney_03@mac.com>
2023-01-15 17:41:19 -0600

    Trying to follow portfolio cues

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,49 +1,83 @@
-import React from "react";
-import "./portfolio.scss";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
-function Portfolio() {
+export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
       <div className="container">
-        <div className="item">
-          <a href="https://tierney03.github.io/OneStop/" target="_onblank">
-            <img src="assets/OneStop.png" alt="screenshot of webpage"></img>
-          </a>
-          <h3>OneStop</h3>
-        </div>
-        <div className="item">
-          <a
-            href="https://evening-stream-22967.herokuapp.com/"
-            target="_onblank"
-          >
-            <img src="assets/GoalE.png" alt="screenshot of webpage"></img>
-          </a>
-          <h3>Goal-E</h3>
-        </div>
-        <div className="item">
-          <a
-            href="https://github.com/tierney03/EmployeeTracker_SQL"
-            target="_onblank"
-          >
-            <img src="assets/SQL.png" alt="screenshot of web recording"></img>
-          </a>
-          <h3>Employee Tracker</h3>
-        </div>
-        <div className="item">
-          <a href="https://github.com/tierney03" target="_onblank">
-            <img src="assets/Github.png" alt="screenshot of webpage"></img>
-          </a>
-          <h3>Github</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
-      <a href="#works">
-        <KeyboardArrowDownIcon className="arrow" />
-      </a>
     </div>
   );
 }
-
-export default Portfolio;
