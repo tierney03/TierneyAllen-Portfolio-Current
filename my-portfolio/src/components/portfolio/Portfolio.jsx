8d7ed9ad8259d29f343a777e629c005256d1bfa4
tierney03 <tierney_03@mac.com>
2023-01-15 15:06:30 -0600

    Refactoring based on my current needs

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./portfolio.scss";
+import { useEffect, useState } from "react";
 
 function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="placeholder"></img>
+          <h3>App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="placeholder"></img>
+          <h3>App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="placeholder"></img>
+          <h3>App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="placeholder"></img>
+          <h3>App</h3>
+        </div>
+      </div>
     </div>
   );
 }
