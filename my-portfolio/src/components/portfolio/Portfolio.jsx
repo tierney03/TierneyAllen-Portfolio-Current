f687fd611c137c659d5df9c1a4e2e56d4b1d3e86
tierney03 <tierney_03@mac.com>
2023-01-15 15:29:26 -0600

    Adding in screenshots and links for portfolio

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -8,19 +8,33 @@ function Portfolio() {
       <h1>Portfolio</h1>
       <div className="container">
         <div className="item">
-          <img src="" alt="placeholder"></img>
+          <a href="https://tierney03.github.io/OneStop/" target="_onblank">
+            <img src="assets/OneStop.png" alt="screenshot of webpage"></img>
+          </a>
+          <h3>OneStop</h3>
+        </div>
+        <div className="item">
+          <a
+            href="https://evening-stream-22967.herokuapp.com/"
+            target="_onblank"
+          >
+            <img src="assets/GoalE.png" alt="screenshot of webpage"></img>
+          </a>
+          <h3>Goal-E</h3>
+        </div>
+        <div className="item">
+          <a
+            href="https://github.com/tierney03/EmployeeTracker_SQL"
+            target="_onblank"
+          >
+            <img src="assets/SQL.png" alt="screenshot of web recording"></img>
+          </a>
           <h3>App</h3>
         </div>
         <div className="item">
-          <img src="" alt="placeholder"></img>
-          <h3>App</h3>
-        </div>
-        <div className="item">
-          <img src="" alt="placeholder"></img>
-          <h3>App</h3>
-        </div>
-        <div className="item">
-          <img src="" alt="placeholder"></img>
+          <a href="https://github.com/tierney03" target="_onblank">
+            <img src="assets/Github.png" alt="screenshot of webpage"></img>
+          </a>
           <h3>App</h3>
         </div>
       </div>
